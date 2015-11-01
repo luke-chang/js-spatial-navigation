a15e44fc27151dc0ce39a653c6c2130ae41bac10
Luke Chang <luke1209@gmail.com>
2015-11-02 00:25:22 +0800

    Add some demo pages

---
+++ demo/js/util.js
@@ -0,0 +1,32 @@
+;(function($) {
+  'use strict';
+
+  $.fn.ensureVisible = function(callback) {
+    var $this = $(this).first();
+    var $parent = $this.parent();
+    var scrollTop = $parent.scrollTop();
+    var scrollBottom = scrollTop + $parent.innerHeight();
+    var marginTop = parseInt($this.css('margin-top'));
+    var marginBottom = parseInt($this.css('margin-bottom'));
+    var top = $this.position().top + scrollTop + marginTop;
+    var bottom = top + $this.outerHeight();
+    var newPosition = null;
+
+    if (scrollTop > top - marginTop) {
+      newPosition = {scrollTop: top - marginTop};
+    } else if (scrollBottom < bottom + marginBottom) {
+      newPosition = {scrollTop: bottom - $parent.innerHeight() + marginBottom};
+    }
+
+    if (newPosition) {
+      $parent.animate(newPosition, {
+        duration: 200,
+        done: callback.bind(this)
+      });
+    } else {
+      callback.call(this);
+    }
+
+    return this;
+  };
+}(jQuery));
