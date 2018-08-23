fedfffb10707b0a5c8af9621934e15ed905b0f61
PaulMaly <paul.malys@gmail.com>
2018-08-23 17:57:50 +0300

    PR#30: For the consistency of the coding style

--- spatial_navigation.js
+++ spatial_navigation.js
@@ -1062,7 +1062,9 @@
         _sections[sectionId] = undefined;
         _sections = extend({}, _sections);
         _sectionCount--;
-        _lastSectionId === sectionId && (_lastSectionId = '');
+        if (_lastSectionId === sectionId) {
+          _lastSectionId = '';
+        }
         return true;
       }
       return false;
